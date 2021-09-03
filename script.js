d80646299bea9a675f8688f954f4f505bed0baae
Guilherme Lino <53881828+DjangoLino12@users.noreply.github.com>
2021-09-02 21:40:32 -0300

    Add files via upload
    up responsivo

--- script.js
+++ script.js
@@ -1,15 +1,2 @@
-const btnIncrementar$ = document.getElementById('btn_incrementar');
-const btnDecrementar$ = document.getElementById('btn_decrementar');
-const p$ = document.getElementById('contador');
 
-let contador = 0;
-
-p$.innerHTML = contador;
-
-btnIncrementar$.addEventListener('click', function (){
-    p$.innerHTML = ++contador;
-});
-
-btnDecrementar$.addEventListener('click', function (){
-    p$.innerHTML = --contador;
-});
+///////////////////////////////////////////////
